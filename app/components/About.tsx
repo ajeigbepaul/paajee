bca47d4e4dbab5f4797c496f98c019d53f478a5c
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-06-14 17:20:26 +0100

    adjusting ui

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -19,10 +19,10 @@ export default function About({ pageInfo }: Props) {
       whileInView={{ opacity: 1 }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-5xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-0 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-4 md:-top-6 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
-      <div className="max-w-full flex flex-col md:flex-row items-center md:items-start justify-center md:py-0  mt-[0px] md:mt-0">
+      <div className="max-w-full absolute top-14 md:top-12 flex flex-col md:flex-row items-center md:items-start justify-center md:py-0  mt-[0px] md:mt-0">
         <motion.img
           initial={{
             x: -200,
@@ -36,7 +36,7 @@ export default function About({ pageInfo }: Props) {
           className="mb-2 mt-4 md:mt-0 md:mb-0 flex-shrink-0 w-20 h-20 rounded-full object-cover md:rounded-lg md:w-32 md:h-48 xl:w-[200px] xl:h-[300px]"
           src={urlFor(pageInfo?.profilePics).url()}
         />
-        <div className="space-y-5 px-2 md:px-10 w-full  max-h-[300px] h-full overflow-y-scroll md:overflow-y-hidden">
+        <div className=" px-2 md:px-10 w-full max-h-[300px] h-full overflow-y-scroll md:overflow-y-hidden">
           <h4 className=" text-sm md:text-xl font-semibold">
             Here is a{" "}
             <span className="underline decoration-[#f68]/50">little</span>{" "}
